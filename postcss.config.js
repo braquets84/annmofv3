4118445b2989021eb6db9691fe151ff5d652a570
braquets84 <braquets84@gmail.com>
2022-01-05 19:15:04 +0100

    build with cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-module.exports = ({env}) => ({
-  plugins: [
-    require('tailwindcss')(),
-    require('postcss-preset-env')(),
-    require('autoprefixer')(),
-    env === 'production' ? require('cssnano')() : false,
-  ]
-});
+module.exports = {
+  plugins: {
+    'tailwindcss':{},
+    'postcss-preset-env':{},
+    'autoprefixer':{},
+    'cssnano':{}
+  }
+}
 
