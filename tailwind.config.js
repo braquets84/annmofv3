78d7c2224ef3412f7a316982fbffd58c02b9e5b0
braquets84 <braquets84@gmail.com>
2022-01-22 22:15:10 +0100

    neumorphism index

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         'bg1-light': '#ffffff',
         'bg1-dark': '#1F2937',
         'bg2-light': '#F9FAFB', /* gray-50 */
-        'bg2-dark': '#1F2937', /* gray-800 */
+        'bg2-dark': '#374151', /* gray-800 */
         'neutral-light': '#000000',
         'neutral-dark': '#ffffff',
         'color': '#86EFAC' /* green-300 */
