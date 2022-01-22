ebf6801ac41ae62787edfa3d86ed1d73117c8ec7
braquets84 <braquets84@gmail.com>
2022-01-22 22:23:51 +0100

    change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        'bg1-light': '#ffffff',
+        'bg1-light': '#E5E7EB',
         'bg1-dark': '#1F2937',
         'bg2-light': '#F9FAFB', /* gray-50 */
         'bg2-dark': '#374151', /* gray-800 */
         'neutral-light': '#000000',
         'neutral-dark': '#ffffff',
-        'color': '#86EFAC' /* green-300 */
+        'color': '#4ADE80' /* green-300 */
       }
     },
   },
