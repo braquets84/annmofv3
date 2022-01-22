c893893b3c789655f910f948f1698fe089a94464
braquets84 <braquets84@gmail.com>
2022-01-22 12:03:03 +0100

    neumorphism

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,njk}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'bg1-light': '#ffffff',
+        'bg1-dark': '#374151',
+        'bg2-light': '#F9FAFB', /* gray-50 */
+        'bg2-dark': '#374151', /* gray-800 */
+        'neutral-light': '#000000',
+        'neutral-dark': '#ffffff',
+        'color': '#86EFAC' /* green-300 */
+      }
+    },
   },
   plugins: [require('tailwindcss-neumorphism')],
 }
