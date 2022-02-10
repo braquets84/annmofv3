67d9403617f6e512e7e36823128c548c5a484155
braquets84 <braquets84@gmail.com>
2022-02-10 21:07:46 +0100

    include youtube lite embed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/assets/images");
   eleventyConfig.addPassthroughCopy("./src/assets/fonts");
+  eleventyConfig.addPassthroughCopy("./src/assets/js");
 
   return {
     dir: {
